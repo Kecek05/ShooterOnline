fb11c28615da911178d83b50560aa04fb8c2e033
Joao Kecek <joaokecek.business@gmail.com>
2025-09-10 10:31:02 -0300

    PlayerStateMachineBrain

--- Assets/Scripts/Game/Player/PlayerManager.cs
+++ Assets/Scripts/Game/Player/PlayerManager.cs
@@ -1,7 +1,5 @@
-using System;
 using KeceK.Input;
 using KeceK.Utils.Components;
-using NUnit.Framework;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -15,16 +13,14 @@ namespace KeceK.Game
         [SerializeField] [Required] GroundCheck _groundCheck;
         [SerializeField] [Required] private Animator _animator;
         [SerializeField] [Required] private InputReaderSO _inputReaderSO;
-        private StateMachine _playerStateMachine;
+        
         
         public CharacterMovement CharacterMovement => _characterMovement;
         public CameraMovement CameraMovement => _cameraMovement;
-
-        private void Awake()
-        {
-            _playerStateMachine = new StateMachine();
-            DefineStateAndTransitions();
-        }
+        public Animator Animator => _animator;
+        
+        public InputReaderSO InputReaderSO => _inputReaderSO;
+        public GroundCheck GroundCheck => _groundCheck;
 
         private void Start()
         {
@@ -32,36 +28,5 @@ namespace KeceK.Game
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
-
-        private void Update()
-        {
-            _playerStateMachine.UpdateState();
-        }
-        
-        private void FixedUpdate()
-        {
-            _playerStateMachine.FixedUpdateState();
-        }
-        
-        private void LateUpdate()
-        {
-            _playerStateMachine.LateUpdateState();
-        }
-
-        private void DefineStateAndTransitions()
-        {
-            var idleState = new IdlePlayerState(this, _animator);
-            var walkState = new WalkPlayerState(this, _animator);
-            var jumpState = new JumpPlayerState(this, _animator);
-            
-            At(idleState, walkState, new FuncPredicate(() => _inputReaderSO.MoveInput != Vector2.zero));
-            At(walkState, idleState, new FuncPredicate(() => _inputReaderSO.MoveInput == Vector2.zero));
-            
-            _playerStateMachine.SetState(idleState);
-        }
-        
-        
-        private void At(IState fromState, IState toState, IPredicate condition) => _playerStateMachine.AddTransition(fromState, toState, condition);
-        private void Any(IState toState, IPredicate condition) => _playerStateMachine.AddAnyTransition(toState, condition);
     }
 }
---
+++ Assets/Scripts/Game/Player/PlayerStateMachineBrain.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace KeceK.Game
+{
+    public class PlayerStateMachineBrain : MonoBehaviour
+    {
+        [Title("References")]
+        [SerializeField] [Required] private PlayerManager _playerManager;
+        
+        private StateMachine _playerStateMachine;
+        
+        private void Awake()
+        {
+            _playerStateMachine = new StateMachine();
+            DefineStateAndTransitions();
+        }
+        
+        private void Update()
+        {
+            _playerStateMachine.UpdateState();
+        }
+        
+        private void FixedUpdate()
+        {
+            _playerStateMachine.FixedUpdateState();
+        }
+        
+        private void LateUpdate()
+        {
+            _playerStateMachine.LateUpdateState();
+        }
+        
+        private void DefineStateAndTransitions()
+        {
+            var idleState = new IdlePlayerState(_playerManager, _playerManager.Animator);
+            var walkState = new WalkPlayerState(_playerManager, _playerManager.Animator);
+            var jumpState = new JumpPlayerState(_playerManager, _playerManager.Animator);
+            
+            // At(idleState, walkState, new FuncPredicate(() => _inputReaderSO.MoveInput != Vector2.zero));
+            // At(walkState, idleState, new FuncPredicate(() => _inputReaderSO.MoveInput == Vector2.zero));
+            
+            _playerStateMachine.SetState(idleState);
+        }
+        
+        private void At(IState fromState, IState toState, IPredicate condition) => _playerStateMachine.AddTransition(fromState, toState, condition);
+        private void Any(IState toState, IPredicate condition) => _playerStateMachine.AddAnyTransition(toState, condition);
+    }
+}
